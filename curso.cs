4a5003428838ea552a9ba09a55ab315ac03d228a
ulifarias7 <fariasulisesnicolas@gmail.com>
2024-10-04 12:12:14 -0300

    cambios

---
+++ curso.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeerData
+{
+    public class curso
+    {
+        public int  CursoId {get ; set;}
+        public string Titulo {get ; set;}
+        public string Descripcion {get ; set;}
+        public System.DateTime FechaPublicacion {get ; set;}
+
+    }
+}
